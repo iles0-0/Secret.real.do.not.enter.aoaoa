4b27db50bb3b40537ff88411796d73ffda364a0d
iles0-0 <ilessbonus@gmail.com>
2026-03-10 02:56:56 +0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 -- [ COMBAT SECTION ]
 local c1 = CombatTab:CreateToggle({
-   Name = "Kill Aura (Universal)",
+   Name = "Kill Aura (Take weapon)",
    CurrentValue = false,
    Callback = function(Value)
        _G.KillAuraActive = Value
